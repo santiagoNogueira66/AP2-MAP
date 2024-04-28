ed0e0295e014a606430ce322c531304c0601c313
santiago nogueira <santiagonogueira66@gmail.com>
2024-04-27 22:22:56 -0300

    migrei para o banco postergree sql, atualizei o metodo de salvar, fiz uma treeview para a listagem dos produtos, falta fazer um metodo para atualizar a treeview após um insert e um metodo para exclusão de produtos

--- telaPrincipal.py
+++ telaPrincipal.py
@@ -1,13 +1,70 @@
-import mysql.connector
-from mysql.connector import  errors
+import psycopg2
 import datetime
 import tkinter as tk
-from tkinter import Frame, Label, Entry, Button
+from tkinter import Frame, Label, Entry, Button, ttk, messagebox
 
+
+class ProdutoModel:
+    @staticmethod
+    def conectar_com_banco():
+        try:
+            conexao = psycopg2.connect(database='DBvendas', host='localhost', user='postgres', password='123456',
+                                       port='5432')
+            cursor = conexao.cursor()
+            return conexao, cursor
+        except psycopg2.Error as err:
+            print("Erro ao conectar ao banco de dados:", err)
+            return None, None
+
+    @staticmethod
+    def salvar_no_Banco(dados):
+        conexao, cursor = ProdutoModel.conectar_com_banco()
+        if conexao and cursor:
+            try:
+                if all(dados):
+                    insert = "INSERT INTO produtos(nome_produto, preco_produto, data_venda) VALUES (%s, %s ,%s)"
+                    cursor.execute(insert, dados)
+                    conexao.commit()
+                    msg = "VENDA FINALIZADA!"
+                    messagebox.showinfo("SUCESSO", msg)
+                    
+                else:
+                    msg = "PREÇO E NOME DO PRODUTO SÃO OBRIGATÓRIOS"
+                    messagebox.showinfo("PREENCHA TODOS OS CAMPOS!", msg)
+            except psycopg2.Error as err:
+                print("Erro ao inserir dados no banco:", err)
+            finally:
+                if cursor:
+                    cursor.close()
+                if conexao:
+                    conexao.close()
+        else:
+            print("Não foi possível conectar ao banco de dados.")
+
+    @staticmethod
+    def obter_dados_do_banco():
+       conexao , cursor = ProdutoModel.conectar_com_banco()
+       if conexao and cursor:
+           try:
+               cursor.execute("SELECT * FROM produtos")
+               return  cursor.fetchall()
+
+           except psycopg2.Error as err:
+               print("erro ao obter dados do banco", err)
+
+           finally:
+               if cursor:
+                   cursor.close()
+               if conexao:
+                   conexao.close()
+
+       else:
+           print("Não foi possível conectar ao banco de dados.")
+           return None
 
 class ProdutoView:
     def __init__(self, root):
-        self.fontepadrao = ("Arial", "15")
+        self.fontepadrao = ("Arial", "12")
         self.root = root
 
         self.primeiroContainer = Frame(root)
@@ -31,6 +88,10 @@ class ProdutoView:
         self.quintoContainer["padx"] = 20
         self.quintoContainer.pack()
 
+        self.sextoContainer = Frame(root)
+        self.sextoContainer["padx"] =20
+        self.sextoContainer.pack()
+
         self.titulo = Label(self.primeiroContainer, text="Gerenciamento de Vendas")
         self.titulo['font'] = ("Arial", "15", "bold")
         self.titulo.pack()
@@ -38,63 +99,69 @@ class ProdutoView:
         self.nomeProdutoLabel = Label(self.segundoContainer, text="Nome do Produto", font=self.fontepadrao)
         self.nomeProdutoLabel.pack(side=tk.LEFT)
 
-        self.nomeProduto = Entry(self.segundoContainer)
-        self.nomeProduto["width"] = 50
-        self.nomeProduto.pack(side=tk.LEFT)
+        self.nomeProdutoEntry = Entry(self.segundoContainer)
+        self.nomeProdutoEntry["width"] = 50
+        self.nomeProdutoEntry.pack(side=tk.LEFT)
 
         self.precoProdutoLabel = Label(self.terceirocontainer, text="Preço do Produto", font=self.fontepadrao)
         self.precoProdutoLabel.pack(side=tk.LEFT)
 
-        self.precoProduto = Entry(self.terceirocontainer)
-        self.precoProduto["width"] = 50
-        self.precoProduto.pack(side=tk.LEFT)
+        self.precoProdutoEntry = Entry(self.terceirocontainer)
+        self.precoProdutoEntry["width"] = 50
+        self.precoProdutoEntry.pack(side=tk.LEFT)
 
-        self.dataLabel = Label(self.quartoContainer, text="Data", font= ("calibri", "15"))
+        self.dataLabel = Label(self.quartoContainer, text="Data", font=("calibri", "15"))
         self.dataLabel.pack(side=tk.LEFT)
 
-        self.dataAtual = datetime.datetime.now().strftime("%y/%m/%d")
-        self.dataProdutoLabel = Label(self.quartoContainer, text=self.dataAtual, font= ("calibri", "15"))
+        self.dataAtual = datetime.datetime.now().strftime("%d/%m/%Y")
+        self.dataProdutoLabel = Label(self.quartoContainer, text=self.dataAtual, font=("calibri", "15"))
         self.dataProdutoLabel.pack(side=tk.LEFT)
 
         self.vender = Button(self.quintoContainer)
         self.vender["text"] = "Vender"
-        self.vender["width"] = 12
-        self.vender["command"] = self.salvar_no_Banco
-        self.vender.pack(pady = 10)
+        self.vender["font"] = ("Arial", "12")
+        self.vender["width"] = 10
+        self.vender["command"] = self.obter_dados
+        self.vender.pack(pady=10)
 
-    def salvar_no_Banco(self):
-        nome_produto = self.nomeProduto.get()
-        preco_produto = self.precoProduto.get()
+        self.minha_lista = ttk.Treeview(self.sextoContainer, height=5, columns=("col1","col2","col3","col4"))
+        self.minha_lista.heading("col1", text="ID")
+        self.minha_lista.heading("col2", text="Nome do produto")
+        self.minha_lista.heading("col3", text="Preço do produto")
+        self.minha_lista.heading("col4", text="Data da venda")
+
+        self.minha_lista.column("col1", width=100, anchor=tk.CENTER)
+        self.minha_lista.column("col2", width=200, anchor=tk.CENTER)
+        self.minha_lista.column("col3", width=300, anchor=tk.CENTER)
+        self.minha_lista.column("col4", width=400, anchor=tk.CENTER)
+        self.minha_lista.pack(expand=True, fill=tk.BOTH)
+
+        self.exibir_dados_do_banco()
+
+    def exibir_dados_do_banco(self):
+        dados_do_banco = ProdutoModel.obter_dados_do_banco()
+        if dados_do_banco:
+            for item in self.minha_lista.get_children():
+                self.minha_lista.delete(item)
+            for row in dados_do_banco:
+                self.minha_lista.insert("", tk.END, values=row)
+
+    def obter_dados(self):
+        nome_produto = self.nomeProdutoEntry.get()
+        preco_produto = self.precoProdutoEntry.get()
         data_venda = self.dataProdutoLabel["text"]
-        ProdutoController.salvar_no_Banco(nome_produto, preco_produto, data_venda)
+
+        dados = (nome_produto, preco_produto, data_venda)
+        ProdutoController.inserir_dados(dados)
 
 
 class ProdutoController:
     @staticmethod
-    def salvar_no_Banco(nome_produto, preco_produto, data_venda):
-     try:
-        con = mysql.connector.connect(host='localhost',database='vendas',user='root',password='coxinha0p0p67')
-        cursor = con.cursor()
+    def inserir_dados(dados):
+        ProdutoModel.salvar_no_Banco(dados)
 
 
-        query = "insert into produtos(nome_produto, preco_produto, data_venda) values (%s, %s ,%s)"
-
-        cursor.execute(query,(nome_produto, preco_produto, data_venda))
-
-        con.commit()
-
-        print(cursor.rowcount,"registros na tabela")
-
-     except mysql.connector.Error as err:
-      print("Erro ao inserir na tabela:", err)
-
-     finally:
-         if con.is_connected():
-            cursor.close()
-            con.close()
-            print("conexão encerrada")
-
-root = tk.Tk()
-ProdutoView(root)
-root.mainloop()
-
+if __name__ == "__main__":
+    root = tk.Tk()
+    ProdutoView(root)
+    root.mainloop()
